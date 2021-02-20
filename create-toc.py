26114c51b06e052302c50d3f35e7cfa2bd644b18
Robert Lohr <4004615+the-codeslinger@users.noreply.github.com>
2021-02-20 10:33:33 +0100

    Write ToC with codecs UTF-8; add config file

--- create-toc.py
+++ create-toc.py
@@ -1,5 +1,5 @@
 # Scan directories for audio files with a given extension and read the
-# meta data from the file's name to create a table of contents JSON
+# meta data from the file"s name to create a table of contents JSON
 # file from that information. This file can later be used by 
 # conversion tools/scripts.
 # 
@@ -15,80 +15,89 @@
 
 import os
 import json
+import codecs
 import argparse
 from pathvalidate import sanitize_filename
 
-ARTIST_TAG_NAME = 'artist'
-ALBUM_TAG_NAME = 'album'
-GENRE_TAG_NAME = 'genre'
-YEAR_TAG_NAME = 'year'
-TRACK_TAG_NAME = 'track'
-TITLE_TAG_NAME = 'title'
-TRACK_LIST_NAME = 'tracks'
-FILENAME_TAG_NAME = 'filename'
-LONG_FILENAME_TAG_NAME = 'long'
-SHORT_FILENAME_TAG_NAME = 'short'
+ARTIST_TAG_NAME = "artist"
+ALBUM_TAG_NAME = "album"
+GENRE_TAG_NAME = "genre"
+YEAR_TAG_NAME = "year"
+TRACK_TAG_NAME = "track"
+TITLE_TAG_NAME = "title"
+TRACK_LIST_NAME = "tracks"
+FILENAME_TAG_NAME = "filename"
+LONG_FILENAME_TAG_NAME = "long"
+SHORT_FILENAME_TAG_NAME = "short"
 
-FORWARD_SLASH_STRING = '/'
-FORWARD_SLASH_CODE = '&47;'
+FORWARD_SLASH_STRING = "/"
+FORWARD_SLASH_CODE = "&47;"
 
-COLON_STRING = ':'
-COLON_CODE = '&58;'
+COLON_STRING = ":"
+COLON_CODE = "&58;"
 
-QUESTION_MARK_STRING = '?'
-QUESTION_MARK_CODE = '&63;'
+QUESTION_MARK_STRING = "?"
+QUESTION_MARK_CODE = "&63;"
 
-BACKSLASH_STRING = '\\'
-BACKSLASH_CODE = '&92;'
+BACKSLASH_STRING = "\\"
+BACKSLASH_CODE = "&92;"
+
+HASH_STRING = "#"
+HASH_CODE = "&35;"
+
+TOC_FILENAME = "ToC.json"
 
 def is_hidden(name):
     return name[0] == "."
 
 def get_artist(file_tags):
-    return file_tags[ARTIST_TAG_NAME] if ARTIST_TAG_NAME in file_tags else ''
+    return file_tags[ARTIST_TAG_NAME] if ARTIST_TAG_NAME in file_tags else ""
 
 def get_album(file_tags):
-    return file_tags[ALBUM_TAG_NAME] if ALBUM_TAG_NAME in file_tags else ''
+    return file_tags[ALBUM_TAG_NAME] if ALBUM_TAG_NAME in file_tags else ""
 
 def get_genre(file_tags):
-    return file_tags[GENRE_TAG_NAME] if GENRE_TAG_NAME in file_tags else ''
+    return file_tags[GENRE_TAG_NAME] if GENRE_TAG_NAME in file_tags else ""
 
 def get_year(file_tags):
-    return file_tags[YEAR_TAG_NAME] if YEAR_TAG_NAME in file_tags else ''
+    return file_tags[YEAR_TAG_NAME] if YEAR_TAG_NAME in file_tags else ""
 
 def get_track(file_tags):
-    return file_tags[TRACK_TAG_NAME].zfill(2) if TRACK_TAG_NAME in file_tags else ''
+    return file_tags[TRACK_TAG_NAME].zfill(2) if TRACK_TAG_NAME in file_tags else ""
 
 def get_title(file_tags):
-    return file_tags[TITLE_TAG_NAME] if TITLE_TAG_NAME in file_tags else ''
+    return file_tags[TITLE_TAG_NAME] if TITLE_TAG_NAME in file_tags else ""
 
-def replace_specials(title):
-    return title \
+def replace_specials(value):
+    return value \
         .replace(FORWARD_SLASH_CODE, FORWARD_SLASH_STRING) \
         .replace(COLON_CODE, COLON_STRING) \
         .replace(QUESTION_MARK_CODE, QUESTION_MARK_STRING) \
-        .replace(BACKSLASH_CODE, BACKSLASH_STRING)
+        .replace(BACKSLASH_CODE, BACKSLASH_STRING) \
+        .replace(HASH_CODE, HASH_STRING)
 
 def simple_filename(file_tags, type):
     track = get_track(file_tags)
     title = get_title(file_tags)
 
-    filename = ''
+    filename = ""
     if track and title:
-        filename = track + "-" + title + "." + type
+        filename = track + " - " + title + "." + type
     elif track and not title:
         filename = track + "." + type
     elif not track and title:
         filename = title + "." + type
     
-    # sanitize_filename removes my coded special characters; I prefer '_' instead
+    # Sanitize_filename removes my coded special characters.
     filename = filename \
-        .replace(' ', '_') \
-        .replace('\'', '') \
-        .replace(FORWARD_SLASH_STRING, '_') \
-        .replace(COLON_STRING, '_') \
-        .replace(QUESTION_MARK_STRING, '_') \
-        .replace(BACKSLASH_STRING, '_')
+        .replace("\"", "") \
+        .replace(",", "") \
+        .replace("!", "") \
+        .replace(FORWARD_SLASH_STRING, "") \
+        .replace(COLON_STRING, "") \
+        .replace(QUESTION_MARK_STRING, "") \
+        .replace(BACKSLASH_STRING, "") \
+        .replace(HASH_STRING, "")
 
     return sanitize_filename(filename)
 
@@ -110,46 +119,50 @@ def remove_redundant_tags(file_tags):
     file_tags.pop(GENRE_TAG_NAME, None)
     file_tags.pop(YEAR_TAG_NAME, None)
     
-def read_tags(filename, format_list, delim):
-    tag_list = filename.split(delim)
-    assert len(tag_list) >= len(format_list), "Number tags in file smaller than expected according to format"
+def read_tags(filename, config):
+    format_list = config["format"]
+    tag_list = filename.split(config["delim"])
+    assert len(tag_list) <= len(format_list), f"Number tags in file {filename} larger than expected according to format"
     
     file_tags = {}
     for index, value in enumerate(tag_list):
         tag_name = format_list[index]
-        if tag_name == TITLE_TAG_NAME:
-            file_tags[tag_name] = replace_specials(value)
-        else:
-            file_tags[tag_name] = value
+        file_tags[tag_name] = replace_specials(value)
 
     return file_tags
 
 def write_toc_file(dir, record_metadata):
-    with open(dir + os.sep + "ToC.json", "w") as json_file:
+    with codecs.open(os.path.join(dir, TOC_FILENAME), "w", encoding="UTF-8") as json_file:
         json.dump(record_metadata, json_file, indent=2)
 
 def rename_files(dir, record_metadata):
     for track_info in record_metadata[TRACK_LIST_NAME]:
         long_name = track_info[FILENAME_TAG_NAME][LONG_FILENAME_TAG_NAME]
         short_name = track_info[FILENAME_TAG_NAME][SHORT_FILENAME_TAG_NAME]
-        os.rename(dir + os.sep + long_name, dir + os.sep + short_name)
+        os.rename(os.path.join(dir, long_name), os.path.join(dir, short_name))
 
-def read_dir(dir, type, format_list, delim):
-    with os.scandir(dir) as iter:
+def read_dir(subdir, config):
+    print(subdir)
+    if os.path.exists(os.path.join(subdir, TOC_FILENAME)):
+        print(f"Folder {subdir} already contains ToC")
+        return
+    
+    with os.scandir(subdir) as iter:
         record_metadata = {
-            ARTIST_TAG_NAME: '',
-            ALBUM_TAG_NAME: '',
-            GENRE_TAG_NAME: '',
-            YEAR_TAG_NAME: '',
+            ARTIST_TAG_NAME: "",
+            ALBUM_TAG_NAME: "",
+            GENRE_TAG_NAME: "",
+            YEAR_TAG_NAME: "",
             TRACK_LIST_NAME: []
         }
         
         for entry in iter:
+            type = config["type"]
             if entry.is_file() and entry.name.endswith("." + type):
                 file = entry.name
                 file_no_ext = file[:-1 * (1 + len(type))]
 
-                file_tags = read_tags(file_no_ext, format_list, delim)
+                file_tags = read_tags(file_no_ext, config)
                 file_tags[FILENAME_TAG_NAME] = {
                     LONG_FILENAME_TAG_NAME: entry.name,
                     SHORT_FILENAME_TAG_NAME: simple_filename(file_tags, type)
@@ -161,43 +174,35 @@ def read_dir(dir, type, format_list, delim):
                 record_metadata[TRACK_LIST_NAME].append(file_tags)
         
         if record_metadata[TRACK_LIST_NAME]:
-            write_toc_file(dir, record_metadata)
-            rename_files(dir, record_metadata)
+            write_toc_file(subdir, record_metadata)
+            rename_files(subdir, record_metadata)
 
-def read_recursive(dir, type, format_list, delim):
-    for subdir, _, _ in os.walk(dir):
-        read_dir(subdir, type, format_list, delim)
+def read_recursive(config):
+    for subdir, _, _ in os.walk(config["source"]):
+        read_dir(subdir, config)
 
-def validate_format_list(format_list):
-    pass
+def read_config(config_path):
+    with codecs.open(config_path, "r", encoding="UTF-8") as f:
+        return json.load(f)
+
+def make_abs_config_path(config):
+    config_path = config
+    if not os.path.isabs(config):
+        config_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), config)
+    return config_path
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-f", "--format", 
-        help="""Format how filenames encode tags. Comma separated list of 
-            'artist', 'album', 'year', 'genre', 'track', 'title'.""")
-    parser.add_argument(
-        "-d", "--delim", 
-        help="Delimiter that separates tags in the filename.")
-    parser.add_argument(
-        "-s", "--source",
-        help="Directory to scan for music files.")
-    parser.add_argument(
-        "-r", "--recurse", 
-        action='store_true',
-        help="Specify if directory shall be scanned recursively.")
-    parser.add_argument(
-        "-t", "--type", 
-        help="Filename extension that specifies the audio files to look for.")
+        "-c", "--config", 
+        help="""Optional configuration file. Defaults to {script-dir}/etc/create-toc.json""",
+        default="etc/create-toc.json")
     return parser.parse_args()
 
 args = parse_args()
 
-format_list = args.format.split(",")
-validate_format_list(format_list)
-
-if args.recurse is True:
-    read_recursive(args.source, args.type, format_list, args.delim)
+config = read_config(make_abs_config_path(args.config))
+if config["recurse"] is True:
+    read_recursive(config)
 else:
-    read_dir(args.source, args.type, format_list, args.delim)
+    read_dir(config["source"], config)
